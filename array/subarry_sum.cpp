20f0bae544820126a80c2007fcc1ebb933a18efb
Balaji <iam.balajirai@gmail.com>
2024-08-04 16:50:27 +0530

    subarray_sum

--- array/subarry_sum.cpp
+++ array/subarry_sum.cpp
@@ -4,51 +4,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-    //Function to find a continuous sub-array which adds up to a given number.
-    vector<int> subarraySum(vector<int>arr, int n, long long s){
-        if(s == 0){
-            for(int i=0; i<n; i++)
-                if(arr[i] == 0)
-                    return vector<int>{i+1,i+1};
-            return vector<int>{-1};
-        }
-        int l = 0, r=0;
-        int curr_sum = 0;
-        while(l<n && r<=n && l<=r){
-            if(curr_sum == s)
-                return vector<int>{l+1,r};
-            if(curr_sum < s)
-                curr_sum += arr[r++];
-            else
-                curr_sum -= arr[l++];
-        }
+//Function to find a continuous sub-array which adds up to a given number.
+vector<int> subarraySum(vector<int>arr, int n, long long s){
+    if(s == 0){
+        for(int i=0; i<n; i++)
+            if(arr[i] == 0)
+                return vector<int>{i+1,i+1};
         return vector<int>{-1};
     }
-};
+
+    int l = 0, r=0;
+    int curr_sum = 0;
+
+    while(l<n && r<=n && l<=r){
+        if(curr_sum == s) return vector<int>{l+1,r};
+        if(curr_sum < s) curr_sum += arr[r++];
+        else curr_sum -= arr[l++];
+    }
+    return vector<int>{-1};
+}
+
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        long long s;
-        cin>>n>>s;
-        vector<int>arr(n);
-        // int arr[n];
-        const int mx = 1e9;
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-        }
-        Solution ob;
-        vector<int>res;
-        res = ob.subarraySum(arr, n, s);
-        
-        for(int i = 0;i<res.size();i++)
-            cout<<res[i]<<" ";
-        cout<<endl;
-        
+    int n;
+    long long s;
+    cin>>n>>s;
+    vector<int>arr(n);
+
+    const int mx = 1e9;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
+
+    vector<int>res;
+    res = subarraySum(arr, n, s);
+    
+    for(int i = 0;i<res.size();i++)
+        cout<<res[i]<<" ";
+    cout<<endl;
+
 	return 0;
 }
